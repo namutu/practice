77f06f6215988f6c7103e0b0696716310112e501
namutu <mikoow3@naver.com>
2021-09-01 00:18:27 +0900

    Q100

---
+++ src/main/java/leetcode/easy/Q100.java
@@ -0,0 +1,39 @@
+package leetcode.easy;
+
+public class Q100 {
+    public static void main(String[] args) {
+
+    }
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        // p and q are both null
+        if (p == null && q == null) return true;
+        // one of p and q is null
+        if (q == null || p == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.right, q.right) &&
+                isSameTree(p.left, q.left);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
